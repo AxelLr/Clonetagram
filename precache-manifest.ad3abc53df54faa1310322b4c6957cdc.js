3882c1e0bc66d54e20b732b39728b8f1bf053869
Axel <axelleonr12@gmail.com>
2020-01-26 23:47:11 -0300

    Updates

---
+++ precache-manifest.ad3abc53df54faa1310322b4c6957cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d099fb68f6390d99de2df31c3170d16a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e82b9b5062e8ac04d026",
+    "url": "/static/css/main.6ba8d068.chunk.css"
+  },
+  {
+    "revision": "d7391d7b8f97c5b442db",
+    "url": "/static/js/2.63bf4569.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/static/js/2.63bf4569.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e82b9b5062e8ac04d026",
+    "url": "/static/js/main.ce67a5d0.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "df803060910f2e04e3ffd98b2d0a6920",
+    "url": "/static/media/image-preview.df803060.png"
+  }
+]);
