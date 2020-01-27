378c8c5b35690632c12cee2c2cdaf9f68c42a65f
Axel <axelleonr12@gmail.com>
2020-01-26 23:54:35 -0300

    Updates

---
+++ precache-manifest.af01c15f1a38610e11f44de040dbeb20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c34decd7e313a9913853bce27ef9505",
+    "url": "/Clonetagram/index.html"
+  },
+  {
+    "revision": "bca235b64a522be43b75",
+    "url": "/Clonetagram/static/css/main.6ba8d068.chunk.css"
+  },
+  {
+    "revision": "d7391d7b8f97c5b442db",
+    "url": "/Clonetagram/static/js/2.63bf4569.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/Clonetagram/static/js/2.63bf4569.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bca235b64a522be43b75",
+    "url": "/Clonetagram/static/js/main.cf7982c2.chunk.js"
+  },
+  {
+    "revision": "e0638d8d35b64dabd0ca",
+    "url": "/Clonetagram/static/js/runtime-main.6e872dba.js"
+  },
+  {
+    "revision": "df803060910f2e04e3ffd98b2d0a6920",
+    "url": "/Clonetagram/static/media/image-preview.df803060.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad3abc53df54faa1310322b4c6957cdc.js"
+  "/Clonetagram/precache-manifest.af01c15f1a38610e11f44de040dbeb20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Clonetagram/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
